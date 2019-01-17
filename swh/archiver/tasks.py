41b3e312616f7ab7f3ad6aa319469cfef64081b2
David Douard <david.douard@sdfa3.org>
2019-01-17 11:57:18 +0100

    Remove debug logging from tasks' code
    since this is now handled by the SWHTask itself.

--- swh/archiver/tasks.py
+++ swh/archiver/tasks.py
@@ -3,27 +3,19 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from swh.scheduler.celery_backend.config import app
+from celery import current_app as app
 
 from .worker import ArchiverWithRetentionPolicyWorker
 from .worker import ArchiverToBackendWorker
 
 
-@app.task(name='swh.archiver.tasks.SWHArchiverWithRetentionPolicyTask',
-          bind=True)
-def archive_with_retention(self, *args, **kwargs):
-    self.log.debug('%s, args=%s, kwargs=%s' % (
-        self.name, args, kwargs))
+@app.task(name=__name__ + '.SWHArchiverWithRetentionPolicyTask')
+def archive_with_retention(*args, **kwargs):
     ArchiverWithRetentionPolicyWorker(*args, **kwargs).run()
-    self.log.debug('%s OK' % (self.name))
 
 
-@app.task(name='swh.archiver.tasks.SWHArchiverToBackendTask',
-          bind=True)
-def archive_to_backend(self, *args, **kwargs):
+@app.task(name=__name__ + '.SWHArchiverToBackendTask')
+def archive_to_backend(*args, **kwargs):
     """Main task that archive a batch of content in the cloud.
     """
-    self.log.debug('%s, args=%s, kwargs=%s' % (
-        self.name, args, kwargs))
     ArchiverToBackendWorker(*args, **kwargs).run()
-    self.log.debug('%s OK' % (self.name))
